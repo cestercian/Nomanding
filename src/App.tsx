5801ea2d2d808253367f3366d726415716b20458
Cestercian <yashafaid@gmail.com>
2025-10-17 00:42:30 +0530

    Home Page Initialisation

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import './App.css'
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
 
 function App() {
-  return (
-      <BrowserRouter>
-          <Routes>
-              <Route path="/" element={<Home />}/>
-          </Routes>
-      </BrowserRouter>
-  )
-}
-
-function Home(){
-    return(
-        <>HI !</>
+    return (
+        <BrowserRouter>
+            <header>
+                <Link className="site-logo" to="/">#VanLife</Link>
+                <nav>
+                    <Link to="/about">About</Link>
+                </nav>
+            </header>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
