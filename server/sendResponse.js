102bca8e7c68ed2da378a36795f8b3acf7b8ae26
Cestercian <yashafaid@gmail.com>
2025-10-20 14:41:47 +0530

    TypeScripting

--- server/sendResponse.js
+++
@@ -1,7 +0,0 @@
-export default function sendResponse(res,statusCode, resType, payload ) {
-
-    res.statusCode = statusCode
-    res.setHeader('content-type', resType )
-    res.end(payload)
-
-}
---
+++ server/sendResponse.ts
@@ -0,0 +1,9 @@
+import {ServerResponse} from "node:http";
+
+export default function sendResponse(res:ServerResponse, statusCode:number, resType:string , payload:string|Buffer ) {
+
+    res.statusCode = statusCode
+    res.setHeader('content-type', resType )
+    res.end(payload)
+
+}
