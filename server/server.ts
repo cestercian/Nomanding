08b9a963e9bc6c7bfa13f445d54f22a81819be7b
Cestercian <yashafaid@gmail.com>
2025-10-20 15:12:12 +0530

    TypeScripting

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,15 @@
-import http from 'node:http'
-import serveStatic from "./serveStatic.ts";
-import sendResponse from "./sendResponse.ts";
+import * as http from 'node:http'
+import serveStatic from "./serveStatic.js";
+import sendResponse from "./sendResponse.js";
 
 const PORT = 8000;
 
-const __dirname = import.meta.dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server =
 
-    http.createServer(async (req,res) => {
+    http.createServer(async (req:http.IncomingMessage,res:http.ServerResponse):Promise<void> => {
 
         if(req.url.startsWith('/vans')){
             return serveStatic(res,__dirname)
