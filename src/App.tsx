e9d4a4cef1f1fe64c889cb959cd3c45ba4832861
Cestercian <yashafaid@gmail.com>
2025-10-20 15:46:42 +0530

    VANS page routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import type {JSX} from "react";
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Vans from "./pages/Vans.tsx";
+
 
 function App() :JSX.Element {
     return (
@@ -10,11 +12,13 @@ function App() :JSX.Element {
                 <Link className="site-logo" to="/">#VanLife</Link>
                 <nav>
                     <Link to="/about">About</Link>
+                    <Link to="/vans">Vans</Link>
                 </nav>
             </header>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/vans" element={<Vans />} />
             </Routes>
         </BrowserRouter>
     )
---
+++ src/pages/Vans.tsx
@@ -0,0 +1,7 @@
+import type {JSX} from 'react'
+
+export default function Vans():JSX.Element {
+    return(
+        <h1>Vanning</h1>
+    )
+}
