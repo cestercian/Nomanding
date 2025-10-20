7d88c2770152c3dd8fd341048e87bd5bf97f3ed0
Cestercian <yashafaid@gmail.com>
2025-10-20 12:41:41 +0530

    API handling

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const __dirname = import.meta.dirname
 const server =
     http.createServer(async (req,res) => {
         if(req.url.startsWith('/vans')){
-            return serveStatic(__dirname)
+            return serveStatic(res,__dirname)
         }
         res.writeHead(200, { "Content-Type": "text/plain" })
         res.end("Server is running ✅ — Try /vans or /vans/1")
--- server/serveStatic.js
+++ server/serveStatic.js
@@ -1,17 +1,23 @@
 import path from "node:path"; import fs from 'node:fs/promises'
+import sendResponse from "./sendResponse.js";
 
 
 
-export default async function serveStatic(cwd) {
+export default async function serveStatic(res,cwd) {
     console.log('serving static')
+
     try{
 
         const fileDirectory = path.join( cwd, 'data' ,'vansData.json' )
-        return JSON.parse(
+        const data =  JSON.parse(
             await fs.readFile(
-                path.join(fileDirectory),
-                "utf-8")
+                path.join(fileDirectory), "utf-8"
+            )
         )
+        sendResponse(res, 200,'appl')
+
     }catch (e) {
         console.log(e)
-        return [] } }
+        return []
+    }
+}
