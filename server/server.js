4ec376660b3ce68a7230b721058e2203ecf6cecc
Cestercian <yashafaid@gmail.com>
2025-10-22 17:53:26 +0530

    CORS

--- server/server.js
+++ server/server.js
@@ -1,19 +1,31 @@
-import http from 'node:http'
+import http from "node:http";
 import serveStatic from "./serveStatic.js";
 import sendResponse from "./sendResponse.js";
 
 const PORT = 8000;
 
-const __dirname = import.meta.dirname
+const __dirname = import.meta.dirname;
 
-const server = http.createServer(async (req,res) => {
+const server = http.createServer(async (req, res) => {
+  if (req.method === "OPTIONS") {
+    res.statusCode = 204;
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS",
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.end();
+    return;
+  }
 
-    if(req.url.startsWith('/vans')){
-        return serveStatic(res,__dirname)
-    }else {
-        sendResponse(res,404,'text/html', 'Not Valid API route')
-    }
+  if (req.url.startsWith("/vans")) {
+    return serveStatic(res, __dirname);
+  } else {
+    sendResponse(res, 404, "text/html", "Not Valid API route");
+  }
+});
 
-})
-
-server.listen(8000,()=>{console.log(`server is connected to ${PORT} at http://localhost:${PORT}`)})
+server.listen(8000, () => {
+  console.log(`server is connected to ${PORT} at http://localhost:${PORT}`);
+});
