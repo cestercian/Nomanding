63f41ffd7509f66d6ad42a3bb5862572434df964
Cestercian <yashafaid@gmail.com>
2025-10-22 18:06:45 +0530

    CSS and Formating 😁

--- src/pages/Vans.tsx
+++ src/pages/Vans.tsx
@@ -17,24 +17,31 @@ export default function Vans():JSX.Element {
     const [vansData, setVansData ] = React.useState<Van[]>([])
 
     React.useEffect(function () {
+
         fetch('http://localhost:8000/vans')
             .then( res => res.json() )
             .then( (resData: Van[]) =>  setVansData(resData) )
+
     },[])
 
-    const vans:JSX.Element[] = vansData.map((van: Van) => (
-        <div key={van.id}>
+    const vanElements :JSX.Element[] = vansData.map((van: Van) => (
+        <div key={van.id} className="van-tile">
             <img src={van.imageUrl} alt={van.name} />
-            <h3>{van.name}</h3>
-            <p>${van.price}/day</p>
+            <div className="van-info">
+                <h3>{van.name}</h3>
+                <p>${van.price}<span>/day</span></p>
+            </div>
+            <i className={`van-type ${van.type} selected`}>{van.type}</i>
         </div>
     ))
 
 
     return(
-        <div>
-            <h1>Vanning</h1>
-            {vans}
+        <div className="van-list-container">
+            <h1>Explore our van options</h1>
+            <div className="van-list">
+                {vanElements}
+            </div>
         </div>
     )
 }
