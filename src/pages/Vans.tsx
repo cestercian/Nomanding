09bddf899442e2c320fa36ad336d97f7a9ded1ba
Cestercian <yashafaid@gmail.com>
2025-10-22 17:53:43 +0530

    Vans API routing

--- src/pages/Vans.tsx
+++ src/pages/Vans.tsx
@@ -1,7 +1,40 @@
-import type {JSX} from 'react'
+import  {type JSX} from 'react'
+import * as React from "react";
+
+
+interface Van {
+    id : string,
+    name : string,
+    price : number,
+    description : string,
+    imageUrl : string,
+    type : string
+
+}
 
 export default function Vans():JSX.Element {
+
+    const [vansData, setVansData ] = React.useState<Van[]>([])
+
+    React.useEffect(function () {
+        fetch('http://localhost:8000/vans')
+            .then( res => res.json() )
+            .then( (resData: Van[]) =>  setVansData(resData) )
+    },[])
+
+    const vans:JSX.Element[] = vansData.map((van: Van) => (
+        <div key={van.id}>
+            <img src={van.imageUrl} alt={van.name} />
+            <h3>{van.name}</h3>
+            <p>${van.price}/day</p>
+        </div>
+    ))
+
+
     return(
-        <h1>Vanning</h1>
+        <div>
+            <h1>Vanning</h1>
+            {vans}
+        </div>
     )
 }
