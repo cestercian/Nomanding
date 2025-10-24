6a6bb61f92faf572991baa4bc9ad66f9db105ea8
Cestercian <yashafaid@gmail.com>
2025-10-24 22:58:35 +0530

    API handeling V2

--- src/pages/VanDetails.tsx
+++ src/pages/VanDetails.tsx
@@ -1,18 +1,40 @@
-import {useEffect} from "react";
+import {useEffect, useState, type JSX} from "react";
+import {useParams} from "react-router-dom";
 
 
-export default function VanDetails() {
+interface Van {
+    id : string,
+    name : string,
+    price : number,
+    description : string,
+    imageUrl : string,
+    type : string
+}
 
+export default function VanDetails():JSX.Element {
+
+    const {id} = useParams<{id : string}>()
+    const [ van , setVans ] = useState <Van | null> (null)
 
     useEffect(function (){
-        fetch("http://localhost:8000/api/vans/1")
+        fetch(`http://localhost:8000/api/vans/${id}`)
             .then( res => res.json())
-            .then( vanData => console.log(vanData))
+            .then( (vanData:Van) => setVans(vanData))
 
-    },[])
-
+    },[id])
 
     return(
-        <h1>VAN DETAILS GOES HERE ::</h1>
+        <div className="van-detail-container">
+            {van ? (
+                <div className="van-detail">
+                    <img src={van.imageUrl} alt={van.name}/>
+                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                    <h2>{van.name}</h2>
+                    <p className="van-price"><span>${van.price}</span>/day</p>
+                    <p>{van.description}</p>
+                    <button className="link-button">Rent this van</button>
+                </div>
+            ) : <h2>Loading...</h2>}
+        </div>
     )
 }
