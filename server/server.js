7a85d65419cc2d531364cd09872b9a88f60468b1
Cestercian <yashafaid@gmail.com>
2025-10-24 18:32:09 +0530

    API handeling V2

--- server/server.js
+++ server/server.js
@@ -7,23 +7,23 @@ const PORT = 8000;
 const __dirname = import.meta.dirname;
 
 const server = http.createServer(async (req, res) => {
-  if (req.method === "OPTIONS") {
-    res.statusCode = 204;
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, OPTIONS",
-    );
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    res.end();
-    return;
-  }
+        if (req.method === "OPTIONS") {
+            res.statusCode = 204;
+            res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+            res.setHeader(
+              "Access-Control-Allow-Methods",
+              "GET, POST, PUT, DELETE, OPTIONS",
+            );
+            res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+            res.end();
+            return;
+        }
 
-  if (req.url.startsWith("/vans")) {
-    return serveStatic(res, __dirname);
-  } else {
-    sendResponse(res, 404, "text/html", "Not Valid API route");
-  }
+        if (req.url.startsWith("/vans") || req.url.startsWith("/api")) {
+            return serveStatic(res, __dirname);
+        } else {
+            sendResponse(res, 404, "text/html", "Not Valid API route");
+        }
 });
 
 server.listen(8000, () => {
