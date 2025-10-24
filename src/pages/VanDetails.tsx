2f32b05105d9b33aa66db4e3f488d829ba477af7
Cestercian <yashafaid@gmail.com>
2025-10-24 18:32:22 +0530

    API handeling V2

--- src/pages/VanDetails.tsx
+++ src/pages/VanDetails.tsx
@@ -1,4 +1,17 @@
+import {useEffect} from "react";
+
+
 export default function VanDetails() {
+
+
+    useEffect(function (){
+        fetch("http://localhost:8000/api/vans/1")
+            .then( res => res.json())
+            .then( vanData => console.log(vanData))
+
+    },[])
+
+
     return(
         <h1>VAN DETAILS GOES HERE ::</h1>
     )
