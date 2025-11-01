ea75e2e546e172e81c68ebe55d9eaa75b4419a89
Cestercian <yashafaid@gmail.com>
2025-11-01 09:39:46 +0530

    Layouting V1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import type {JSX} from "react";
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans.tsx";
 import VanDetails from "./pages/VanDetails.tsx";
+import Layout from "./components/Layout.tsx";
 
 
 function App() :JSX.Element {
     return (
         <BrowserRouter>
-            <header>
-                <Link className="site-logo" to="/">#VanLife</Link>
-                <nav>
-                    <Link to="/about">About</Link>
-                    <Link to="/vans">Vans</Link>
-                </nav>
-            </header>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/vans" element={<Vans />} />
-                <Route path="/vans/:id" element={<VanDetails/>}></Route>
+                <Route element={<Layout />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/vans" element={<Vans />} />
+                    <Route path="/vans/:id" element={<VanDetails/>} />
+                </Route>
             </Routes>
         </BrowserRouter>
     )
