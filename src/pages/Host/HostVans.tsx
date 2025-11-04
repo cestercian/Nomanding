dcbb4cff8ffb0bce95fd3eabe59740fb49ded739
Cestercian <yashafaid@gmail.com>
2025-11-04 17:42:59 +0530

    Hosted Vans Page TYPED

--- src/pages/Host/HostVans.tsx
+++ src/pages/Host/HostVans.tsx
@@ -6,35 +6,32 @@ interface HostedVan {
     name : string,
     price : number,
     imageUrl : string
+    hostId: number
 }
 
 export default function HostVans():JSX.Element {
 
-    const [ hostedVans, setHostedVans ] = React.useState([])
+    const [ hostedVans, setHostedVans ] = React.useState<HostedVan[]>([])
 
     useEffect(function () {
         fetch(`http://localhost:8000/api/vans/`)
             .then(res => res.json())
-            .then( (hostedVansData) => {
-                const selectedHostVans =  hostedVansData.filter( v => v.hostId === 123  )
+            .then( (hostedVansData :HostedVan[] ) => {
+                const selectedHostVans : HostedVan[] | [] =  hostedVansData.filter( ( v : HostedVan )  => v.hostId === 123  )
                 setHostedVans (selectedHostVans)
             } )
-            .then(console.log("servernworking"))
             .catch((err)=> console.log(err))
 
     },[])
 
-    console.log(hostedVans)
 
-    const hostedVansList :JSX.Element  = hostedVans.map( (van) => (
+    const hostedVansList :JSX.Element[]  = hostedVans.map( (van) => (
         <>
             <h1>{van.name}</h1>
             <h2>{van.price}</h2>
             <p>{van.imageUrl}</p>
         </>
     ) )
-
-    console.log(hostedVansList)
 
     return (
         <>
