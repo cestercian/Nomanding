fc8aa6ab2d81f0b108876e6ba1dbd248eeacfe2c
Cestercian <yashafaid@gmail.com>
2025-11-30 12:52:49 +0530

    Error Handling

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,5 +2,12 @@ import type { Van } from "../types/types.ts";
 
 export default async function getVans() : Promise<Van[]> {
     const res : Response = await fetch("http://localhost:8000/api/vans");
+    if(!res.ok){
+        throw {
+            message: "Failed to fetch vans",
+            statusText: res.statusText,
+            status:res.status
+        }
+    }
     return await res.json();
 }
