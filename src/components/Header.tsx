92260bf1c9fe9f6133d5b9ce5328ed85664d5ab7
Cestercian <yashafaid@gmail.com>
2025-12-03 01:32:33 +0530

    Login Page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import {Link} from "react-router-dom";
 import type {JSX} from "react";
-
+import imageUrl from "../assets/images/avatar-icon.png"
 
 export default function Header(): JSX.Element {
     return (
@@ -10,6 +10,12 @@ export default function Header(): JSX.Element {
                 <Link to="/host">Host</Link>
                 <Link to="/about">About</Link>
                 <Link to="/vans">Vans</Link>
+                <Link to="login" className="login-link">
+                    <img
+                        src={imageUrl}
+                        className="login-icon"
+                     alt={'avatar-logo'}/>
+                </Link>    
             </nav>
         </header>
     )
